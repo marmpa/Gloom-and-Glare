d88c19b9e8f02b283bc3a2023793cf186109a2c4
tofantaraki <tommyliaros97@gmail.com>
2018-01-06 18:35:11 +0200

    Add empty project
    oriste agorina! Den pisteuo na mporei na ta dei kanenas allos..!?

---
+++ Tester_G&G/Assets/LevelsMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelsMenu : MonoBehaviour {
+
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+    public Button[] levelButtons;
+
+    void Start()
+    {
+        int levelReached = PlayerPrefs.GetInt("levelReached", 1);
+        for (int i = 0; i < levelButtons.Length; i++)
+        {
+            if (i + 1 > levelReached)
+            {
+                levelButtons[i].interactable = false;
+            }
+        }
+    }
+
+    public void PlayGame(int sceneIndex)
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+        }
+
+    }
+}
---
+++ Tester_G&G/Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
