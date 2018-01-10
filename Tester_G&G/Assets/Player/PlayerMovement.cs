875a48787bfc166df3da39128003ad5fa52afe09
marmpa <marmpa@gmail.com>
2018-01-10 13:53:06 +0200

    Added new things \n Like prefab ball which work for now and some \n box colliders

---
+++ Tester_G&G/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (SwipeManager.Instance.IsSwiping (SwipeDirection.Up)) 
+		{
+			GetComponent<Rigidbody> ().AddForce(Vector3.up * 400);
+		}
+        Rigidbody body = GetComponent<Rigidbody>();
+        
+        
+        
+            if(Input.GetMouseButton(0) && Input.mousePosition.x < Screen.width/2)
+            {
+                body.AddForce(Vector3.left *2);
+            Debug.Log("Hello");
+        }
+            else if(Input.GetMouseButton(0))
+            {
+                body.AddForce(Vector3.right *2);
+            }
+        
+        
+
+    
+
+}
+}
---
+++ Tester_G&G/Assets/Player/SwipeManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum SwipeDirection
+{
+	None = 0,
+	Left = 1,
+	Right = 2,
+	Up = 4,
+	Down = 8,
+	LeftUp=5,
+	RightUp=6
+}
+
+public class SwipeManager : MonoBehaviour 
+{
+	private static SwipeManager instance;
+	public static SwipeManager Instance{ get { return instance; } }
+
+	public SwipeDirection Direction{ set; get; }
+
+	private Vector3 touchPosition;
+	private float swipeResistanceX = 50.0f;
+	private float swipeResistanceY = 100.0f;
+
+	public bool IsSwiping(SwipeDirection dir)
+	{
+		return (Direction & dir) == dir;
+	}﻿
+
+	void Start()
+	{
+		instance = this;
+	}
+
+	void Update () 
+	{
+		Direction = SwipeDirection.None;
+
+		if (Input.GetMouseButtonDown (0)) 
+		{
+			touchPosition = Input.mousePosition;
+		}
+
+		if (Input.GetMouseButtonUp (0)) 
+		{
+			Vector2 deltaSwipe = touchPosition - Input.mousePosition;
+
+			if (Mathf.Abs (deltaSwipe.x) > swipeResistanceX) 
+			{
+				//Κάνω swipe στο X axis
+				Direction |= (deltaSwipe.x < 0)?SwipeDirection.Right : SwipeDirection.Left;
+			}
+
+			if (Mathf.Abs (deltaSwipe.y) > swipeResistanceY) 
+			{
+				//Κάνω swipe στο Υ axis
+				Direction |= (deltaSwipe.y < 0)?SwipeDirection.Up : SwipeDirection.Down;
+			}
+		}
+
+	}
+}
+									
