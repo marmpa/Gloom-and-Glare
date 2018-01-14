69d6447c4cd8c4f53ec8b8742ca0a209d1d6a5d6
tofantaraki <tommyliaros97@gmail.com>
2018-01-14 11:58:09 +0200

    Leaderboards + facebook missing

---
+++ Tester_G&G/Assets/Platforms/Scripts/FollowCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FollowCamera : MonoBehaviour {
+
+    public TextMeshPro timerText;
+    float seconds = 0f;
+    float minutes = 0f;
+    float newSeconds = 0f;
+    void Start()
+    {
+        newSeconds = Time.realtimeSinceStartup;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        seconds = Time.realtimeSinceStartup - newSeconds;
+        if (seconds > 60)
+        {
+            seconds = 0;
+            newSeconds = Time.realtimeSinceStartup;
+            minutes++;
+        }
+
+
+        timerText.SetText("Timer-> 0" + (int)minutes + ":" + (int)seconds);
+
+    }
+}
